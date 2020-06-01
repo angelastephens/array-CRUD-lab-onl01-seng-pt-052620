b639cde4c873f74770fba0021010cdfc6d1bf4fe
Angela Stephens <angelastephens317@gmail.com>
2020-06-01 04:44:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["ape","bird","cat","dog"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push
 end
 
 def add_element_to_start_of_array(array, element)
