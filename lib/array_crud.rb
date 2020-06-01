f031f1751d5c63491fe9594afb1dc52106ea9fab
Angela Stephens <angelastephens317@gmail.com>
2020-06-01 04:48:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  array= ["ape","bird","cat","dog"]
-  array.push("elephant")
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
